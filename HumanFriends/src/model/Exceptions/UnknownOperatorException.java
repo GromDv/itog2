cc5ef7237d60b5c08d0fc6d4f9a9bb33881e097b
GromDV <sup-bx@yandex.ru>
2023-07-28 12:49:19 +0300

    Correction1

--- HumanFriends/src/model/Exceptions/UnknownOperatorException.java
+++
@@ -1,7 +0,0 @@
-package model.Exceptions;
-
-public class UnknownOperatorException extends Exception {
-    public UnknownOperatorException(String message) {
-        super(String.format("В выражении есть непонятный знак: %s, попробуйте заново.", message));
-    }
-}
